562b678f25863624002c1a43f9b94a4392e0fb74
manas1803 <manasmonupant@gmail.com>
2024-11-18 12:09:22 +0530

    Imports and exports

---
+++ Lesson 5/Calculate/calculateSum.js
@@ -0,0 +1,6 @@
+function calculateSum(a, b) {
+  const add = a + b;
+  console.log("Sum is ", add);
+}
+
+module.exports = { calculateSum };
---
+++ Lesson 5/app.js
@@ -0,0 +1,5 @@
+require("./somefile");
+const { calculateSum, calculateMultiply } = require("./Calculate");
+
+calculateMultiply(10,20)
+calculateSum(10,20)
