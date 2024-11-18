76fbad8752194e943cbcbdfea6abca0b45664979
manas1803 <manasmonupant@gmail.com>
2024-11-18 20:40:29 +0530

    Lesson 6

---
+++ Lesson 6/app.js
@@ -0,0 +1,5 @@
+const { x } = require("./learningRequire");
+
+console.log("Hello From app js");
+const num = 100;
+console.log(num - x);
---
+++ Lesson 6/learningRequire.js
@@ -0,0 +1,5 @@
+console.log("Hello from learning require")
+
+const x = 10;
+
+module.exports = {x}
